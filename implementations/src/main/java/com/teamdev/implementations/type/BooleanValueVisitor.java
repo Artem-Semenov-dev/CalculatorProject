a2d1af7363edffc6a7628a95f7b32f181eef45ff
arenot <2mega.artemsem@ukr.net>
2022-07-18 10:52:52 +0300

    Add isDouble and isBoolean check for types in BazaScript.

--- implementations/src/main/java/com/teamdev/implementations/type/BooleanValueVisitor.java
+++ implementations/src/main/java/com/teamdev/implementations/type/BooleanValueVisitor.java
@@ -4,15 +4,29 @@ public class BooleanValueVisitor implements ValueVisitor{
 
     private boolean booleanValue;
 
+    private boolean isBoolean;
+
+    private final boolean throwExceptionPermission;
+
+    private BooleanValueVisitor(boolean throwExceptionPermission) {
+        this.throwExceptionPermission = throwExceptionPermission;
+    }
+
     @Override
     public void visit(DoubleValue value) {
-        throw new IllegalArgumentException("Type mismatch: expected boolean but double provided");
+        if (throwExceptionPermission) {
+            throw new IllegalArgumentException("Type mismatch: expected boolean but double provided");
+        }
+
+        isBoolean = false;
     }
 
     @Override
     public void visit(BooleanValue value) {
 
         booleanValue = value.getBooleanValue();
+
+        isBoolean = true;
     }
 
     private boolean getBooleanValue(){
@@ -20,10 +34,20 @@ public class BooleanValueVisitor implements ValueVisitor{
     }
 
     public static Boolean read(Value value){
-        BooleanValueVisitor booleanValueVisitor = new BooleanValueVisitor();
+        BooleanValueVisitor booleanValueVisitor = new BooleanValueVisitor(true);
 
         value.accept(booleanValueVisitor);
 
         return booleanValueVisitor.getBooleanValue();
+    }
+
+    public static Boolean isBoolean(Value value) {
+
+        BooleanValueVisitor visitor = new BooleanValueVisitor(false);
+
+        value.accept(visitor);
+
+        return visitor.isBoolean;
+
     }
 }
--- implementations/src/main/java/com/teamdev/implementations/type/DoubleValueVisitor.java
+++ implementations/src/main/java/com/teamdev/implementations/type/DoubleValueVisitor.java
@@ -1,29 +1,53 @@
 package com.teamdev.implementations.type;
 
-public class DoubleValueVisitor implements ValueVisitor{
-    private double doubleValue;
+public class DoubleValueVisitor implements ValueVisitor {
+    private Double doubleValue;
+
+    private boolean isDouble;
+
+    private final boolean throwExceptionPermission;
+
+    private DoubleValueVisitor(boolean throwExceptionPermission) {
+        this.throwExceptionPermission = throwExceptionPermission;
+    }
 
     @Override
     public void visit(DoubleValue value) {
 
         doubleValue = value.getValue();
+
+        isDouble = true;
     }
 
     @Override
     public void visit(BooleanValue value) {
 
-        throw new IllegalArgumentException("Type mismatch: expected double but boolean provided");
+        if (throwExceptionPermission) {
+            throw new IllegalArgumentException("Type mismatch: expected double but boolean provided");
+        }
+
+        isDouble = false;
     }
 
     private double getDoubleValue() {
         return doubleValue;
     }
 
-    public static Double read(Value value){
-        DoubleValueVisitor doubleVisitor = new DoubleValueVisitor();
+    public static Double read(Value value) {
+        DoubleValueVisitor doubleVisitor = new DoubleValueVisitor(true);
 
         value.accept(doubleVisitor);
 
         return doubleVisitor.getDoubleValue();
+    }
+
+    public static Boolean isDouble(Value value) {
+
+        DoubleValueVisitor visitor = new DoubleValueVisitor(false);
+
+        value.accept(visitor);
+
+        return visitor.isDouble;
+
     }
 }
